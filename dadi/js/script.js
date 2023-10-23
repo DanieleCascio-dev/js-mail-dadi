f15386257bdc02cc521b174f8e89c88061bcb2a3
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-10-23 17:10:12 +0200

    add: style and button to dice

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,20 +1,34 @@
+const computerElem = document.querySelector(".computer");
+const playerElem = document.querySelector(".player");
+const winner = document.querySelector(".winner");
+const tryAgain = document.getElementById("try");
+console.log(tryAgain);
+
 //Random number
 
 //Computer
 const computerNum = Math.floor(Math.random() * 7);
 console.log(computerNum);
-
+computerElem.innerHTML = `<h2>Computer</h2><div class="dice lightcoral">${computerNum}</div>`;
 //Player
 const playerNum = Math.floor(Math.random() * 7);
 console.log(playerNum);
+playerElem.innerHTML = `<h2>Player</h2><div class="dice lightblue">${playerNum}</div>`;
 
+let winnerMess = "";
 //Condition
 if (playerNum > computerNum) {
-  console.log("Il giocatore ha vinto!");
+  winnerMess = "Player win!";
 } else if (playerNum < computerNum) {
-  console.log("Il Computer ha vinto!");
-} else if (playerNum === computerNum) {
-  console.log("Pareggio!");
-  alert("Pronto a ritirare i dadi!?");
-  location.reload();
+  winnerMess = "Computer win!";
+} else {
+  winnerMess = "Draw!";
+  /*  alert("Pronto a ritirare i dadi!?");
+  location.reload(); */
 }
+console.log(winnerMess);
+winner.innerHTML = `<h2>${winnerMess}</h2>`;
+
+tryAgain.addEventListener("click", function () {
+  location.reload();
+});
